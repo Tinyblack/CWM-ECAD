ea58d851ce263352127caaa19b782bae58a5958f
Tinyblack <23023013+Tinyblack@users.noreply.github.com>
2021-04-19 10:16:28 +0100

    Complete solution to Ex4

--- Ex4/top.v
+++ Ex4/top.v
@@ -16,3 +16,120 @@
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns/100ps
+
+module dice (
+    input clk,
+    input rst,
+    input button,
+    output reg [2:0]throw
+);
+    reg [2:0]state_reg;
+    reg [2:0]state_next;
+
+    always @(posedge clk or negedge rst) begin  
+        if (~rst) 
+            begin
+                state_reg=3'b000;      
+            end
+        else 
+            begin
+                state_reg<=state_next;
+            end       
+    end
+
+    always @(button or state_reg or state_next) begin
+        state_next=state_reg;
+        case (state_reg)
+            3'b000:
+            begin
+                throw=3'b000;
+                state_next=3'b001;
+            end
+            3'b001:
+            begin
+                throw=3'b001;
+                if (button==1'b0) 
+                    begin 
+                        state_next=3'b001;
+                    end
+                else
+                    begin
+                        state_next=3'b010;
+                    end
+            end 
+            3'b010:
+            begin
+                throw=3'b010;
+                if (button==1'b0) 
+                        state_next=3'b010;
+                else
+                        state_next=3'b011;
+            end
+            3'b011:
+            begin
+                throw=3'b011;
+                if (button==1'b0) 
+                    begin 
+                        state_next=3'b011;
+                    end
+                else
+                    begin
+                        state_next=3'b100;
+                    end
+            end
+            3'b100:
+            begin
+                throw=3'b100;
+                if (button==1'b0) 
+                    begin 
+                        state_next=3'b100;
+                    end
+                else
+                    begin
+                        state_next=3'b101;
+                    end
+            end
+            3'b101:
+            begin
+                throw=3'b101;
+                if (button==1'b0) 
+                    begin 
+                        state_next=3'b101;
+                    end
+                else
+                    begin
+                        state_next=3'b110;
+                    end
+            end
+            3'b110:
+            begin
+                throw=3'b110;
+                if (button==1'b0) 
+                    begin 
+                        state_next=3'b110;
+                    end
+                else
+                    begin
+                        state_next=3'b001;
+                    end
+            end
+            3'b111:
+            begin
+                throw=3'b111;
+                if (button==1'b0) 
+                    begin 
+                        state_next=3'b001;
+                    end
+                else
+                    begin
+                        state_next=3'b010;
+                    end
+            end
+            default: 
+                begin
+                    throw=3'b000;
+                end
+        endcase
+    end  
+endmodule
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -7,3 +7,99 @@
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 10ps
+
+module top_tb (
+);
+    //Parameters
+    parameter CLK_PERIOD = 10;
+
+    //Regitsers and wires
+    reg clk;
+    reg rst;
+    reg button;
+    reg err;
+    reg [2:0]throw_pre;
+    wire [2:0]throw;
+
+    //Clock generation
+    initial begin
+       clk = 1'b0;
+       forever begin
+         #(CLK_PERIOD/2) clk=~clk;
+       end
+    end
+
+    //Rest generation
+    initial begin
+        rst=1;
+        #1
+        rst=0;
+        #1
+        rst=1;
+        if (throw!=3'b000) begin
+            $display("*** TEST FAILED! Reset Value Wrong! Actual out=%d ***",throw);
+            err=1;
+        end
+    end
+
+    //
+    initial begin
+        #3
+        throw_pre=throw;
+        #2
+        forever begin
+            #(CLK_PERIOD/5)
+            throw_pre=throw;
+        end
+    end
+
+    //Button generation
+    initial begin
+        button=1;
+        #306
+        button=0;
+    end
+
+    //Simulation logic
+    initial begin
+        err=0;
+        #3
+        #2
+        forever begin
+            #(CLK_PERIOD)
+            if (button==0 && throw!=throw_pre) begin
+                $display("*** TEST FAILED! STOP FAILED. Actual out=%d ***",throw);
+                err=1;
+            end
+
+            if (button==1 && (throw>6 || throw<1)) begin
+                $display("*** TEST FAILED! THROW FAILED. Actual out=%d ***",throw);
+                err=1;
+            end
+            
+        end
+    end
+
+    //Finish test, check for success
+    initial begin
+    #600 //This contorl the time of simulation
+        if (err==0) begin
+            $display("*** TEST PASSED! :) ***");
+        end
+    $finish;
+    end
+
+
+
+    //Instantiate module
+    dice top(
+        .clk(clk),
+        .rst(rst),
+        .button(button),
+        .throw(throw)
+    );
+
+
+    
+endmodule
