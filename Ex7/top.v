247c34c7b10bd6957743b1d44ba8513e93386c42
Tinyblack <23023013+Tinyblack@users.noreply.github.com>
2021-04-20 17:10:23 +0100

    A complete solution to Ex7

--- Ex7/top.v
+++ Ex7/top.v
@@ -10,7 +10,23 @@
 //           clk, a[2:0], b[2:0], read
 //
 //  outputs:
-//           result[4:0]
+//           result[5:0]
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 10ps
 
+module multiplier (
+    input clk,
+    input [2:0]a,
+    input [2:0]b,
+    input read,
+    output [5:0]result
+);
+    
+blk_mem_gen_0 memory (
+  .clka(clk),    // input wire clka
+  .ena(read),      // input wire ena
+  .addra({b,a}),  // input wire [5 : 0] addra
+  .douta(result)  // output wire [5 : 0] douta
+);
+endmodule
--- Ex7/top_tb.v
+++ Ex7/top_tb.v
@@ -7,3 +7,96 @@
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 10ps
+
+module top_tb (
+);
+    //Parameters
+    parameter CLK_PERIOD = 10;
+
+    //Regitsers and wires
+
+    reg [2:0]a;
+    reg [2:0]b;
+    reg clk;
+    reg read;
+    reg [5:0]pre;
+    reg [5:0]product;
+    wire [5:0]result;
+    reg err;
+
+    //Clock generation
+    initial begin
+       clk = 1'b0;
+       forever begin
+         #(CLK_PERIOD/2) clk=~clk;
+         if (clk==1'b1) begin
+             pre=product;
+             product=a*b;
+         end
+       end
+    end
+
+    //Select generation
+    initial begin
+        read=0;
+        #105
+        read=1;
+    end
+
+    initial begin
+        a=3'b000;
+        forever begin
+            #(CLK_PERIOD)
+            a=a+3'b001;
+        end
+    end
+
+    initial begin
+        b=3'b000;
+        forever begin
+            #(CLK_PERIOD-3)
+            b=b+3'b001;
+        end
+    end
+
+    //Save previous value
+    initial begin
+        #5
+        #2
+        #140
+        forever begin
+            #(CLK_PERIOD)
+            if (read==1 && result!=pre) begin
+                $display("*** TEST FAILED! Actual out=%d, Expected out=%d ***",result,pre);
+                err=1;
+            end
+        end
+    end
+
+    //Simulation logic
+    initial begin
+        err=0;
+    end
+
+    //Finish test, check for success
+    initial begin //This maximum simulation time is set in .tcl file.
+    #600 
+        if (err==0) begin
+            $display("*** TEST PASSED! :) ***");
+        end
+    $finish;
+    end
+
+
+
+    //Instantiate module
+    multiplier top(
+        .clk(clk),
+        .a(a),
+        .b(b),
+        .read(read),
+        .result(result)
+    );
+    
+  endmodule  
